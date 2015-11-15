3d35ce654960dc677ec55e938f79356b474f3fac
Needrom <1264507247@qq.com>
2015-11-15 23:21:49 +0800

    the previous one was miss some config head

---
+++ stm32F103VCT6+GL21_fontLibrary+touch/Project/Template/RVMDK/Obj/delay.d
@@ -0,0 +1,6 @@
+.\obj\delay.o: delay\delay.c
+.\obj\delay.o: ..\..\..\Libraries\CMSIS\Core\CM3\core_cm3.h
+.\obj\delay.o: .\sys\sys.h
+.\obj\delay.o: ..\stm32f10x.h
+.\obj\delay.o: ..\..\..\Libraries\CMSIS\Core\CM3\system_stm32f10x.h
+.\obj\delay.o: D:\keil\ARM\ARMCC\Bin\..\include\stdint.h
